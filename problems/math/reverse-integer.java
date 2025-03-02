3f0e7b9f6bf2d80628826b30ae464cf30cc27197
Chirag <chirag@Chirags-MacBook-Pro.local>
2025-03-02 23:14:04 +0530

    move all under problems

---
+++ problems/math/reverse-integer.java
@@ -0,0 +1,26 @@
+// LeetCode #7: Reverse Integer (Medium) - https://leetcode.com/problems/reverse-integer
+class Solution {
+    public int reverse(int x) {
+        try{
+            int res = 0;
+            int remainder = 0;
+            boolean isNegative = x < 0 ? true : false;
+            if(isNegative) {
+                x *= -1;
+            }
+            while(x > 0){
+                if(res > Integer.MAX_VALUE / 10) {
+                    return  0;
+                }
+                res *= 10;
+                remainder = x % 10;
+                res += remainder;
+                x = x/10;
+            }
+
+            return isNegative? res * -1: res;
+        } catch(NumberFormatException n) {
+            return 0;
+        }
+    }
+}
